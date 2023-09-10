4ee191b8420b9244e9b301d2deb872c239005f1b
Sebastian Olsson <sebelino7@gmail.com>
2023-09-10 14:28:48 +0200

    feat(kind): Add sample go app

---
+++ kind/myapp/myapp.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"html"
+	"log"
+	"net/http"
+)
+
+func main() {
+	fmt.Println("Starting server...")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		endpoint := html.EscapeString(r.URL.Path)
+		fmt.Fprintf(w, "Hello, %q", endpoint)
+	})
+	http.HandleFunc("/hi", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hi")
+	})
+	log.Fatal(http.ListenAndServe(":8081", nil))
+}
