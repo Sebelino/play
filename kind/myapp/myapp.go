144626ab554b582b803c4c693b740e901bd057fd
Sebastian Olsson <sebelino7@gmail.com>
2023-09-12 20:56:04 +0200

    feat: Allow specifying HTTP_PORT, with sane default

--- kind/myapp/myapp.go
+++ kind/myapp/myapp.go
@@ -5,10 +5,28 @@ import (
 	"html"
 	"log"
 	"net/http"
+	"os"
+	"strconv"
 )
+
+func lookupOrDefaultTo(envvar string, defaultValue string) string {
+	value, present := os.LookupEnv(envvar)
+	if present {
+		return value
+	}
+	fmt.Printf("%s not set. Using default value: %s\n", envvar, defaultValue)
+	return defaultValue
+}
 
 func main() {
 	fmt.Println("Starting server...")
+
+	httpPortStr := lookupOrDefaultTo("HTTP_PORT", "8000")
+	httpPort, err := strconv.Atoi(httpPortStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		endpoint := html.EscapeString(r.URL.Path)
 		fmt.Fprintf(w, "Hello, %q", endpoint)
@@ -21,5 +39,5 @@ func main() {
 		fmt.Println(pong)
 		fmt.Fprintf(w, pong)
 	})
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", httpPort), nil))
 }
